e555cbe9c6bc93690039a62c738d3ab5f91fa72f
Ecky Putrady <eckyputrady@gmail.com>
2015-02-07 23:00:43 +0700

    Clean up all extra things and will use gulp

--- app/scripts/background.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  'use strict';
-  chrome.runtime.onInstalled.addListener(function(details) {
-    return console.log('previousVersion', details.previousVersion);
-  });
-
-  console.log('\'Allo \'Allo! Event Page');
-
-}).call(this);
